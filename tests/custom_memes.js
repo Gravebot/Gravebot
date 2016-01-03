5f130471c4eb8085a7834a9f2c66a43f6b0beb12
Dustin Blackman <dev@dustinblackman.com>
2016-01-03 17:04:10 -0500

    add tests and coverage

---
+++ tests/custom_memes.js
@@ -0,0 +1,109 @@
+import chai from 'chai';
+import customMemes from '../lib/custom_memes';
+
+chai.should();
+
+describe('custom_memes', () => {
+  describe('ayylmao', () => {
+    it('should return the correct imgur url and local file', done => {
+      let completed = 0;
+
+      function sendMessage(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('http://i.imgur.com/m7NaGVx.png');
+
+        completed++;
+        if (completed === 2) return done();
+      }
+
+      function sendFile(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('./images/Ayylmao.png');
+
+        completed++;
+        if (completed === 2) return done();
+      }
+      customMemes.ayylmao({
+        sendMessage,
+        sendFile
+      }, {channel: 'test'});
+    });
+  });
+
+  describe('chillenmyb', () => {
+    it('should return the correct local file', done => {
+      function sendFile(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('./images/Chillenmyb.jpg');
+        done();
+      }
+      customMemes.chillenmyb({sendFile}, {channel: 'test'});
+    });
+  });
+
+  describe('feelsgoodman', () => {
+    it('should return the correct local file', done => {
+      function sendFile(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('./images/Feelsgoodman.png');
+        done();
+      }
+      customMemes.feelsgoodman({sendFile}, {channel: 'test'});
+    });
+  });
+
+  describe('kappa', () => {
+    it('should return the correct local file', done => {
+      function sendFile(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('./images/Kappa.png');
+        done();
+      }
+      customMemes.kappa({sendFile}, {channel: 'test'});
+    });
+  });
+
+  describe('kappahd', () => {
+    it('should return the correct local file', done => {
+      function sendFile(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('./images/Kappahd.png');
+        done();
+      }
+      customMemes.kappahd({sendFile}, {channel: 'test'});
+    });
+  });
+
+  describe('skeltal', () => {
+    it('should return the correct imgur url', done => {
+      function sendMessage(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('http://i.imgur.com/ZX79Q4S.png');
+        done();
+      }
+      customMemes.skeltal({sendMessage}, {channel: 'test'});
+    });
+  });
+
+  describe('starwars4', () => {
+    it('should return the correct imgur url', done => {
+      function sendMessage(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('http://i.imgur.com/l9VKWWF.png');
+        done();
+      }
+      customMemes.starwars4({sendMessage}, {channel: 'test'});
+    });
+  });
+
+  describe('starwars5', () => {
+    it('should return the correct imgur url', done => {
+      function sendMessage(channel, res) {
+        channel.should.equal('test');
+        res.should.equal('http://i.imgur.com/eCpwo6J.png');
+        done();
+      }
+      customMemes.starwars5({sendMessage}, {channel: 'test'});
+    });
+  });
+});
