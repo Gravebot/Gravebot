e1dd8519a5678768d8931cdd3719454ab98c5973
Dustin Blackman <dev@dustinblackman.com>
2016-01-05 18:23:26 -0500

    finish wolfram

--- tests/wolfram.js
+++ tests/wolfram.js
@@ -1,10 +1,9 @@
 import chai from 'chai';
 import fs from 'fs';
 import nock from 'nock';
+import nconf from 'nconf';
 import path from 'path';
 import wolfram from '../lib/wolfram';
-
-import CONFIG from '../config';
 
 chai.should();
 const res_fixture = fs.readFileSync(path.join(__dirname, './fixtures/wolfram.xml'));
@@ -17,9 +16,10 @@ describe('wolfram', () => {
       done();
     }
 
+    nconf.set('WOLFRAM_KEY', '123');
     nock.cleanAll();
     nock('http://api.wolframalpha.com')
-      .get(`/v2/query?units=metric&format=plaintext%2Cimage&primary=true&appid=${CONFIG.wolfram_api}&input=Who%20is%20the%20prime%20minister%20of%20Canada%3F`)
+      .get(`/v2/query?units=metric&format=plaintext%2Cimage&primary=true&appid=${nconf.get('WOLFRAM_KEY')}&input=Who%20is%20the%20prime%20minister%20of%20Canada%3F`)
       .reply(200, res_fixture);
 
     wolfram.wolfram({sendMessage}, {channel: 'test'}, 'Who is the prime minister of Canada?');
