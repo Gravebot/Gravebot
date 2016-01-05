8a8b420b284fa613e2c31e55c5948da94de4630c
Dustin Blackman <dev@dustinblackman.com>
2016-01-05 18:05:21 -0500

    support communicating with the bot using mentions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Client as Discord } from 'discord.js';
 import express from 'express';
 import nconf from 'nconf';
+import R from 'ramda';
 
 import './lib/config/init';
 import commands from './lib';
@@ -25,13 +26,24 @@ bot.on('disconnected', () => {
 });
 
 bot.on('message', msg => {
-  // Checks if the message is a command
+  // Checks for PREFIX
   if (msg.content[0] === nconf.get('PREFIX')) {
     let command = msg.content.toLowerCase().split(' ')[0].substring(1);
     let suffix = msg.content.substring(command.length + 2);
     let cmd = commands[command];
 
     if (cmd) cmd(bot, msg, suffix);
+    return;
+  }
+
+  // Checks if bot was mentioned
+  if (msg.mentions.get('username', nconf.get('USERNAME'))) {
+    let msg_split = msg.content.split(' ');
+    let suffix = R.join(' ', R.slice(2, msg_split.length, msg_split));
+    let cmd = commands[msg_split[1]];
+
+    if (cmd) cmd(bot, msg, suffix);
+    return;
   }
 });
 
