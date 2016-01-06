1514c85bb98ae30279b40c2e08622dc4e96ae428
Dustin Blackman <dev@dustinblackman.com>
2016-01-05 22:34:57 -0500

    Merge branch 'master' into feature/wolfram
    # Conflicts:
    #	config.js.example

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { Client as Discord } from 'discord.js';
+import { Client as Discord, PMChannel } from 'discord.js';
 import express from 'express';
 import nconf from 'nconf';
+import R from 'ramda';
 
 import './lib/config/init';
 import commands from './lib';
@@ -25,13 +26,34 @@ bot.on('disconnected', () => {
 });
 
 bot.on('message', msg => {
-  // Checks if the message is a command
+  // Checks for PREFIX
   if (msg.content[0] === nconf.get('PREFIX')) {
     let command = msg.content.toLowerCase().split(' ')[0].substring(1);
     let suffix = msg.content.substring(command.length + 2);
     let cmd = commands[command];
 
     if (cmd) cmd(bot, msg, suffix);
+    return;
+  }
+
+  // Checks if bot was mentioned
+  if (msg.isMentioned(bot.user)) {
+    let msg_split = msg.content.split(' ');
+    let suffix = R.join(' ', R.slice(2, msg_split.length, msg_split));
+    let cmd = commands[msg_split[1]];
+
+    if (cmd) cmd(bot, msg, suffix);
+    return;
+  }
+
+  // Check personal messages
+  if (msg.channel instanceof PMChannel) {
+    let msg_split = msg.content.split(' ');
+    let suffix = R.join(' ', R.slice(1, msg_split.length, msg_split));
+    let cmd = commands[msg_split[0]];
+
+    if (cmd) cmd(bot, msg, suffix);
+    return;
   }
 });
 
