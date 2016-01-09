2b7ae8e352c488a193c26a40cced57547f37b084
Dustin Blackman <dev@dustinblackman.com>
2016-01-09 17:24:17 -0500

    add tests

---
+++ tests/_helpers.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import R from 'ramda';
+
+export function loadFixtures(fixtures_path, encoding = 'utf8') {
+  var loaded_fixtures = {};
+
+  R.forEach(file_name => {
+    let fixture_ext = path.extname(file_name);
+    let fixture_name = path.basename(file_name, fixture_ext);
+    let fixture_path = path.join(fixtures_path, file_name);
+
+    loaded_fixtures[fixture_name] = fs.readFileSync(fixture_path, encoding);
+    if (fixture_ext === '.json') loaded_fixtures[fixture_name] = JSON.parse(loaded_fixtures[fixture_name]);
+  }, fs.readdirSync(fixtures_path));
+
+  return loaded_fixtures;
+}
