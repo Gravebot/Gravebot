601da2b197326e90467954400f3c586a2b1effaf
Dustin Blackman <dev@dustinblackman.com>
2016-01-11 20:06:49 -0500

    save items locally

--- data/index.js
+++ data/index.js
@@ -3,6 +3,7 @@ import _drama from './drama';
 import _eightball from './eightball';
 import _emoji from './emoji';
 import _lol_champs from './lol_champs';
+import _lol_items from './lol_items';
 import _memes from './memes';
 import _quotes from './quotes';
 
@@ -12,5 +13,6 @@ export let drama = _drama;
 export let eightball = _eightball;
 export let emoji = _emoji;
 export let lol_champs = _lol_champs;
+export let lol_items = _lol_items;
 export let memes = _memes;
 export let quotes = _quotes;
