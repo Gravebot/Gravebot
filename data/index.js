1890538f4d78d0943787afa6799258eae20ef1dc
Dustin Blackman <dev@dustinblackman.com>
2016-01-11 19:45:35 -0500

    save champs locally

--- data/index.js
+++ data/index.js
@@ -2,6 +2,7 @@ import _choices from './choices';
 import _drama from './drama';
 import _eightball from './eightball';
 import _emoji from './emoji';
+import _lol_champs from './lol_champs';
 import _memes from './memes';
 import _quotes from './quotes';
 
@@ -10,5 +11,6 @@ export let choices = _choices;
 export let drama = _drama;
 export let eightball = _eightball;
 export let emoji = _emoji;
+export let lol_champs = _lol_champs;
 export let memes = _memes;
 export let quotes = _quotes;
