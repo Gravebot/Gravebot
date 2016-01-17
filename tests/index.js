25f0825598ac2561ffbd4cfcd8b98e941d0a3273
Dustin Blackman <dev@dustinblackman.com>
2016-01-16 20:46:53 -0500

    set default port for tests

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,12 @@
 import glob from 'glob';
+import nconf from 'nconf';
 import R from 'ramda';
 import '../lib/config/init';
 import '../lib/config/express';
 import '../lib/config/phantom';
+
+
+nconf.set('PORT', 5000);
 
 let glob_options = {
   realpath: true,
