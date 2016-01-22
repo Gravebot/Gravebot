c811282e0603e2686858a31dd3a848d6c73ab5d7
unknown <etp12@pitt.edu>
2016-01-22 08:35:24 -0500

    fixed line spacings in Pastebin files

--- tests/pastebin.js
+++ tests/pastebin.js
@@ -1,5 +1,4 @@
 import chai from 'chai';
-import fs from 'fs';
 import nconf from 'nconf';
 import nock from 'nock';
 import pastebin from '../lib/pastebin';
@@ -7,7 +6,7 @@ import pastebin from '../lib/pastebin';
 chai.should();
 
 describe('pastebin', () => {
-    const key = nconf.get('PASTEBIN_KEY');
+  const key = nconf.get('PASTEBIN_KEY');
 
   it('should return a url to a paste', done => {
     function sendMessage(channel, res) {
@@ -21,6 +20,6 @@ describe('pastebin', () => {
       .post('/api/api_post.php', {api_dev_key: key, api_option: 'paste', api_paste_code: 'test'})
       .reply(200, 'http://pastebin.com/EcYgmBpk');
 
-      pastebin.makePaste({sendMessage}, {channel: 'test'}, 'test');
+    pastebin.makePaste({sendMessage}, {channel: 'test'}, 'test');
   });
 });
