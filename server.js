4c859a7df6a24f9505190e16f0775c6b6b0649d4
Dustin Blackman <dev@dustinblackman.com>
2016-02-01 21:34:50 -0500

    fix infinite loop error

--- server.js
+++ server.js
@@ -66,6 +66,8 @@ bot.on('disconnected', () => {
 });
 
 function onMessage(msg) {
+  if (bot.user.username === msg.author.username) return;
+
   // Checks for PREFIX
   if (msg.content[0] === nconf.get('PREFIX')) {
     let command = msg.content.toLowerCase().split(' ')[0].substring(1);
