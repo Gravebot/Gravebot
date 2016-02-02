49a0730f59f37d12c43593ff20bdc8d5da85f515
Dustin Blackman <dev@dustinblackman.com>
2016-02-02 16:16:00 -0500

    fix pastebin tests

--- tests/useful/pastebin.js
+++ tests/useful/pastebin.js
@@ -8,7 +8,9 @@ import pastebin from '../../lib/useful/pastebin';
 chai.should();
 
 describe('pastebin', () => {
-  const key = nconf.get('PASTEBIN_KEY');
+  before(() => {
+    nconf.set('PASTEBIN_KEY', '123');
+  });
 
   it('should return a url to a paste', done => {
     function sendMessage(channel, res) {
@@ -19,7 +21,7 @@ describe('pastebin', () => {
 
     nock.cleanAll();
     nock('http://pastebin.com')
-      .post('/api/api_post.php', {api_dev_key: key, api_option: 'paste', api_paste_code: 'test'})
+      .post('/api/api_post.php', {api_dev_key: nconf.get('PASTEBIN_KEY'), api_option: 'paste', api_paste_code: 'test'})
       .reply(200, 'http://pastebin.com/EcYgmBpk');
 
     pastebin.paste({sendMessage}, {channel: 'test'}, 'test');
