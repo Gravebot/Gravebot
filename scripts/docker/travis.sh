350f1e448f59b73b74dfd454705e506d2ec45df4
Dustin Blackman <dev@dustinblackman.com>
2016-02-07 23:04:04 -0500

    update travis builds

--- scripts/docker/travis.sh
+++ scripts/docker/travis.sh
@@ -3,7 +3,10 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ -n "$DOCKER_EMAIL" ]
 then
   docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASS"
   export REPO=gravebot/gravebot
-  docker build --build-arg NODE_VERSION=$(cat package.json | json engines.node) --build-arg NPM_VERSION=$(cat package.json | json engines.npm) -f Dockerfile -t $REPO:$COMMIT .
+  export NODE_VERSION=$(cat package.json | json engines.node)
+  export NPM_VERSION=$(cat package.json | json engines.npm)
+  echo $NODE_VERSION $NPM_VERSION
+  docker build --build-arg NODE_VERSION=$NODE_VERSION --build-arg NPM_VERSION=$NPM_VERSION -f Dockerfile -t $REPO:$COMMIT .
   docker tag -f $REPO:$COMMIT $REPO:latest
   docker push $REPO
 else
