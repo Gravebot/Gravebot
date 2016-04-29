5fbcc29df4d5ec00080f4383424ae3d53eaca3b6
PhunStyle <jager1993@gmail.com>
2016-04-29 20:10:38 +0200

    further e621 integration

--- src/commands/lewd/e621.js
+++ src/commands/lewd/e621.js
@@ -28,11 +28,37 @@ function _makeRequest(options) {
     });
 }
 
+function latest(suffix) {
+  const query = suffix.toLowerCase().replace('tags ', '');
+  if (query == '') return Promise.resolve(`No tags were supplied :warning:`);
+  const options = {
+    url: `https://e621.net/post/index.json?tags=${query}`,
+    qs: {
+      limit: 1
+    }
+  };
+
+  return _makeRequest(options)
+    .then(body => {
+      let file = R.pluck('file_url')(body);
+      let id = R.pluck('id')(body);
+      let artist = R.pluck('artist')(body);
+      let height = R.pluck('height')(body);
+      let width = R.pluck('width')(body);
+      let score = R.pluck('score')(body);
+      let faves = R.pluck('fav_count')(body);
+      let title = `This is the latest image on e621 for: **${query}**`;
+      let reply = R.join('\n', R.prepend(title, file));
+      let data = (`\n\`ID: ${id}\` \`Artist: ${artist}\` \`Resolution: ${width} x ${height}\` \`Score: ${score}\` \`Favorites: ${faves}\``);
+      let output = R.join('', R.prepend(reply, data));
+      return output
+    })
+}
+
+
 function tags(suffix) {
   const query = suffix.toLowerCase().replace('tags ', '');
-
-  if (query == '') return Promise.resolve(`You did not supply any tags :warning:`);
-
+  if (query == '') return Promise.resolve(`No tags were supplied :warning:`);
   const options = {
     url: `https://e621.net/post/index.json?tags=${query}`,
     qs: {
@@ -61,6 +87,7 @@ export default {
   e6: (client, evt, suffix, lang) => {
     const command = suffix.toLowerCase().split(' ')[0];
 
+    if (command === 'latest') return latest(suffix);
     if (command === 'tags') return tags(suffix);
     if (command !== 'tags' || command !== 'random') return tags(suffix);
   }
