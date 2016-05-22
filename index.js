e2a4c03b244407514f0ec9fefbda9e4a4e6a6bd5
Dustin Blackman <dev@dustinblackman.com>
2016-05-22 08:13:00 -0400

    change boot messages

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const chalk = require('chalk');
 const moment = require('moment');
-console.log(chalk.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Booting...`));
 
 if (process.env.DEV) {
+  console.log(chalk.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Booting using dev...`));
   require('babel-register');
   require('./src/');
 } else {
+  console.log(chalk.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Booting...`));
   require('./dist');
 }
