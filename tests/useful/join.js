5839326f46ad1a7f80073d310e7af8afa0415b72
Dustin Blackman <dev@dustinblackman.com>
2016-05-30 09:32:13 -0400

    fix tests

--- tests/useful/join.js
+++ tests/useful/join.js
@@ -5,7 +5,8 @@ describe('join/join-server', () => {
   it('should join execute joinServer and respond with with a success message', done => {
     function reply(res) {
       res.should.equal(`To invite me to your server, click the link below and select a server.
-Only users with **Manage Server** permission in that server are able to invite me to it. You may remove some of the permissons if you wish, but be warned it may break current and upcoming features.
+Only users with **Manage Server** permission in that server are able to invite me to it.
+You may remove some of the permissions if you wish, but be warned it may break current and upcoming features.
 https://discordapp.com/oauth2/authorize?&client_id=123&scope=bot&permissions=125982`);
       done();
     }
