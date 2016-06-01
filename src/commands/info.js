d26327a482809862f140971a9f54afb4848ce8e3
Gravestorm <risrobe@gmail.com>
2016-06-01 14:45:14 +0300

    Feedback clarification

--- src/commands/info.js
+++ src/commands/info.js
@@ -97,9 +97,9 @@ User Limit: ${channel.user_limit}
 }
 
 function feedback(client, evt, suffix, lang) {
-  if (!nconf.get('FEEDBACK_CHANNEL')) return Promise.resolve(T('feedback_setup', lang));
+  if (!nconf.get('FEEDBACK_CHANNEL_ID')) return Promise.resolve(T('feedback_setup', lang));
   if (!suffix) return Promise.resolve(T('feedback_usage', lang));
-  client.Channels.find(channel => channel.id === nconf.get('FEEDBACK_CHANNEL')).sendMessage(`**(${evt.message.author.username}) [${evt.message.author.id}]\n(${evt.message.guild.name}) [${evt.message.guild.id}]**\n${suffix.replace(/([@#*_~`])/g, '\\$1')}`);
+  client.Channels.find(channel => channel.id === nconf.get('FEEDBACK_CHANNEL_ID')).sendMessage(`**(${evt.message.author.username}) [${evt.message.author.id}]\n(${evt.message.guild.name}) [${evt.message.guild.id}]**\n${suffix.replace(/([@#*_~`])/g, '\\$1')}`);
 }
 
 function ping() {
