db9b3848cd3648c1894cbd88cf399779ad60155b
Gravestorm <risrobe@gmail.com>
2016-06-03 18:03:45 +0300

    Cleanup

--- src/commands/useful/define.js
+++ src/commands/useful/define.js
@@ -1,40 +1,40 @@
 import Promise from 'bluebird';
 import defineword from 'define-word';
-// import R from 'ramda';
+import R from 'ramda';
 
 import T from '../../translate';
 
 
-function define(suffix, lang) {
+function define(client, evt, suffix, lang) {
   if (!suffix) return Promise.resolve(T('define_usage', lang));
-  let word = defineword.define(suffix);
+  const word = defineword.define(suffix);
   if (!word.definitions.length) return Promise.resolve(`${T('define_error', lang)}: ${suffix}`);
   let text = '';
   text += suffix + ' is a ' + word.type + '\n';
-  for (let i = 0; i < word.definitions.length; i++) {
-    text += i + 1 + '. ' + word.definitions[i] + '\n';
-  }
+  R.forEach(definition => {
+    text += definition + '\n';
+  }, word.definitions);
   return Promise.resolve(text);
 }
 
-function synonym(suffix, lang) {
+function synonyms(client, evt, suffix, lang) {
   if (!suffix) return Promise.resolve(T('synonym_usage', lang));
-  let word = defineword.synonyms(suffix);
-  console.log(word.length);
+  const word = defineword.synonyms(suffix);
   if (!defineword.synonyms(suffix)) return Promise.resolve(`I couldn\'t find a synonym for: ${suffix}`);
   let text = '';
   text += suffix + ' synonyms:\n';
-  for (let i = 0; i < word.length; i++) {
-    text += word[i] + ', ';
-  }
+  R.forEach(word => {
+    text += R.join(', ', word);
+  }, word.length);
   return Promise.resolve(text);
 }
 
 export default {
   d: define,
   define,
-  synonym,
-  synonyms: synonym
+  definition: define,
+  synonym: synonyms,
+  synonyms
 };
 
 export const help = {
