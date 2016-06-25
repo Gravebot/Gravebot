37158b4747e17b7741ac688bae9b43106369b634
Dustin Blackman <dev@dustinblackman.com>
2016-06-24 20:10:14 -0400

    cleanup

--- src/datadog.js
+++ src/datadog.js
@@ -13,6 +13,5 @@ if (nconf.get('DATADOG_APIKEY') && nconf.get('DATADOG_APPKEY')) {
 }
 
 export default function send(metric, value) {
-  if (dogapi.client.api_key && dogapi.client.app_key) console.log('Submitting metrics');
   if (dogapi.client.api_key && dogapi.client.app_key) dogapi.metric.send(metric, value);
 }
